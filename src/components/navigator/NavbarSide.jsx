c6923c075c4347f9899aff00035a1ae00daf79eb
Basilius Tengang <basilius.tengang-2021@fst.unair.ac.id>
2024-09-12 23:23:04 +0700

    added sidebar collapse transition

--- src/components/navigator/NavbarSide.jsx
+++ src/components/navigator/NavbarSide.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavbarSide.css';
 
 export default function NavbarSide() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   const menus = ['Dashboard', 'Contact', 'Broadcast', 'Template', 'Statistic'];
 
   return (
-    <div className="navbar-side">
+    <div className={`navbar-side ${isCollapsed ? 'collapsed' : ''}`}>
       {menus.map((menu, index) => (
-        <div key={index} className="nav-item">
+        <div key={index} className={`nav-item ${isCollapsed ? 'collapsed' : ''}`}>
           <img
             className="nav-icon"
             src={`src/assets/icons/${menu.toLowerCase()}.svg`}
+            alt={menu}
           />
-          <p className="nav-title">{menu}</p>
+          {/* Hide title when collapsed */}
+          {!isCollapsed && <p className="nav-title">{menu}</p>}{' '}
         </div>
       ))}
-      <div className='sidebar-thingy'>
-      <img src="src\assets\icons\arrow_right_double.svg" />
+      <div className="sidebar-thingy" onClick={toggleSidebar}>
+        <img
+          src={`src/assets/icons/arrow_${
+            isCollapsed ? 'right' : 'left'
+          }_double.svg`}
+          alt="toggle-sidebar"
+        />
       </div>
     </div>
   );
