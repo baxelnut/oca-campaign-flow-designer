9c1debc011a3384166378a55f9baa24097459d9d
Basilius Tengang <basilius.tengang-2021@fst.unair.ac.id>
2024-09-22 03:41:15 +0700

    minor adjustments

--- src/features/template/create-template/whatsapp-template/base/LeadingBody.jsx
+++ src/features/template/create-template/whatsapp-template/base/LeadingBody.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css'; // Import Quill styles
+import 'react-quill/dist/quill.snow.css'; 
 
 export default function LeadingBody() {
   const [value, setValue] = useState('');
